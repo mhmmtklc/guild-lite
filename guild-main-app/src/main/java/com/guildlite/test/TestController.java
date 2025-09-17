9dc33681fa440957d4755302ae9d9f984641f597
Muhammet KILIÇ <muhammetkilic@yahoo.com>
2025-09-18 02:24:29 +0300

    code fix

--- guild-main-app/src/main/java/com/guildlite/test/TestController.java
+++ guild-main-app/src/main/java/com/guildlite/test/TestController.java
@@ -2,7 +2,6 @@ package com.guildlite.test;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
-
-    @PostMapping("/hello")
-    public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello from test controller!");
-    }
-
-    @GetMapping("/hello")
-    public ResponseEntity<String> helloGet() {
-        return ResponseEntity.ok("Hello GET working!");
-    }
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping() {
